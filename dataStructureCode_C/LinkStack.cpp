ac202988ef47bd2864b262870467d1f96fcfe45a
wenboshicn <ynu20201120443@mail.ynu.edu.cn>
2022-01-23 21:00:08 +0800

    顺序栈的基本操作

---
+++ dataStructureCode_C/LinkStack.cpp
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+typedef int ElemType;
+typedef struct LNode
+{
+    ElemType data;
+    ElemType * next;
+}LNode , *LinkList;
+
+typedef struct LinkStack
+{
+    LNode * bottom;
+    LNode * top;
+}LinkStack;
+
+Head
+
+
+int main(){
+    
+    return 0;
+}
