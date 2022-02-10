952499049c1d211f4556b43ceb454c5c848c8557
wenboshicn <ynu20201120443@mail.ynu.edu.cn>
2022-02-10 22:26:14 +0800

    保留了演示时的代码

--- dataStructureCode_C/Array.cpp
+++ dataStructureCode_C/Array.cpp
@@ -57,8 +57,8 @@
         }
 
 */
-
 #define MAX_ARRAY_DIM 8 // 数组维数最大为8
+
 typedef int ElemType;
 typedef struct
 {
@@ -200,6 +200,7 @@ int AssignAll(Array & A, ElemType e){
     return 1;
 }
 
+
 int main(){
     Array A;
     InitArray(A, 2, 3, 4); // 初始化一个二维3*4的数组
@@ -214,3 +215,170 @@ int main(){
     DestroyArray(A);
     return 0;
 }
+
+
+
+
+//下面是演示时使用的代码：
+// typedef int ElemType;
+// typedef struct Array
+// {
+//     int dim ;  //维数
+//     ElemType *base; // 空间基址
+//     int *bounds; // 维界一维数组，bounds[i]表示第i+1维的维界,单元的个数
+//     int *constants; // 数组映像函数常量数组（存的是每个维度的一个单元的长度）constants[i]表示第i+1维的一个单元的长度（ElemType个数），constants[i] = bounds[i+1] * constants[i+1] , constants[dim-1] = 1（表示一个ElemType）bounds和constants[dim-1]是容易知道的，因此求constans需要从dim-1开始求，下标从dim-1到0
+//     int elemTotal; // 表示数组中所有数据元素的个数
+// }Array;
+
+
+
+// //变长参数表 ...中暗示的参数个数为dim个，定义3*4的数组就用InitArray(A, 100, 100, 200),3*4*5  InitArray(A, dim, 3, 4, 5)
+// int InitArray(Array & A, int dim, ...){
+//     if(dim < 1 || dim > MAX_ARRAY_DIM){
+//         return 0;
+//     }
+//     A.dim = dim;
+//     A.bounds = (int *)malloc(dim * sizeof(int));
+//     if(!A.bounds){
+//         printf("malloc error\n");
+//         return 0;
+//     }
+//     //对变长参数表进行操作
+//     va_list ap;
+//     va_start(ap, dim);
+//     // ap中的值int , double, int
+//     //从ap中取值指定的类型int , float ,int
+//     A.elemTotal = 1;
+//     for(int i = 0 ; i < dim ; i++){
+//         A.bounds[i] = va_arg(ap, int);
+//         if(A.bounds[i] <= 0){
+//             return 0;
+//         }
+//         A.elemTotal *= A.bounds[i];
+//     }
+//     va_end(ap);
+//     //对变长参数表的操作完成
+//     A.base = (ElemType *)malloc(A.elemTotal*sizeof(ElemType));
+//     if(!A.base){
+//         printf("malloc error\n");
+//         return 0;
+//     }
+//     //求constants
+//     A.constants = (int*)malloc(dim*sizeof(int));
+//     if(!A.constants){
+//         printf("malloc error\n");
+//         return 0;
+//     }
+//     A.constants[A.dim - 1] = 1;
+//     for(int i = A.dim - 2; i >= 0 ; i--){
+//         A.constants[i] = A.bounds[i + 1]*A.constants[i + 1];
+//     }
+//     return 1;
+// }
+// //求相对地址reAddress, 不是给主函数调用的，给其他非主函数调用
+// int Locate(Array A, va_list ap , int & reAddress){
+//     int index = 0;
+//     for(int i = 0 ; i < A.dim ; i++){
+//         index = va_arg(ap , int);
+//         //合法性判断
+//         if( index < 0 || index >= A.bounds[i]){
+//             return 0;
+//         }
+//         reAddress += A.constants[i]*index;
+//     }
+//     return 1;
+// }
+
+// //访问值函数
+// int Value(Array A , ElemType & e, ...){
+//     va_list ap;
+//     va_start(ap, e);
+//     int reAddress = 0;
+//     if(Locate(A,ap,reAddress) == 0 ){
+//         va_end(ap);
+//         return 0;
+//     }else{
+//         e = A.base[reAddress];  // e = *(A.base+reAddress);
+//         va_end(ap);
+//         return 1;
+//     }
+   
+// }
+
+// //给对应位置赋e的值
+// int Assign(Array & A, ElemType e, ...){
+//     va_list ap;
+//     va_start(ap, e);
+//     int reAddress = 0;
+//     if(Locate(A,ap,reAddress) == 0 ){
+//         va_end(ap);
+//         return 0;
+//     }else{
+//         *(A.base+reAddress) = e;   // A.base[reAddress] = e;
+//         va_end(ap);
+//         return 1;
+//     }
+// }
+// //展现一维和二维数组
+// int DisplayArray(Array A){
+//     if(A.dim > 2){
+//         printf("无法展示");
+//         return 0;
+//     }else if(A.dim == 1){
+//         for (int i = 0; i < A.elemTotal; i++)
+//         {
+//             printf("%d\t", A.base[i]);
+//         }
+//         printf("\n");
+//     }else{
+//         //dim == 2:
+//         for(int i = 0; i < A.elemTotal; i++){
+//             printf("%d\t",A.base[i]);
+//             if(i % A.bounds[1] == A.bounds[1] - 1){
+//                 printf("\n");
+//             }
+//         }
+//     }
+//     return 1;
+// }
+
+
+// int AssignAll(Array & A, ElemType e){
+//     if(A.dim == 0){
+//         printf("数组为空\n");
+//         return 0;
+//     }
+//     for(int i = 0; i < A.elemTotal; i++){
+//         A.base[i] = e;
+//     }
+//     return 1;
+// }
+
+// int DestroyArray(Array & A){
+//     if(!A.base){
+//         return 0;
+//     }
+//     free(A.base);
+//     A.base = NULL;
+//     if(!A.bounds){
+//         return 0;
+//     }
+//     free(A.bounds);
+//     A.bounds = NULL;
+//     if(!A.constants){
+//         return 0;
+//     }
+//     free(A.constants);
+//     A.constants = NULL;
+//     A.dim = 0;
+//     A.elemTotal = 0;
+//     return 1;
+// }
+
+
+
+
+
+
+
+
